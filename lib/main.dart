0e0553f4e52ae223ffc76a97da64297a24111c1f
MitkuDubale38 <mitkudubale3864@gmail.com>
2023-12-06 12:54:22 -0500

    results page overflow fix progress

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,29 @@ void main() async {
           debugShowCheckedModeBanner: false,
           title: "Driving Exam",
           initialRoute: initialRoute,
-          theme: ThemeData.light(), // Set a default theme
+          theme: ThemeData(
+            primarySwatch: tealSwatch,
+          ),
           getPages: AppPages.routes,
         );
       },
     ),
   );
 }
+
+
+const MaterialColor tealSwatch = MaterialColor(
+  0xFF016A70, // Primary color value
+  <int, Color>{
+    50: Color(0xFFE0F2F1),
+    100: Color(0xFFB2DFDB),
+    200: Color(0xFF80CBC4),
+    300: Color(0xFF4DB6AC),
+    400: Color(0xFF26A69A),
+    500: Color(0xFF009688), // Primary color value
+    600: Color(0xFF00897B),
+    700: Color(0xFF00796B),
+    800: Color(0xFF00695C),
+    900: Color(0xFF004D40),
+  },
+);
