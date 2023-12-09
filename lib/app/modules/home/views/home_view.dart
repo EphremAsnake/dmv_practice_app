8bc5058dc23f45a582c3bc2aab1120ab01b23d0c
MitkuDubale38 <mitkudubale3864@gmail.com>
2023-12-09 13:42:23 -0500

    status bar bug fix test progress

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,14 +11,19 @@ import 'package:get/get.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:shimmer/shimmer.dart';
 import '../controllers/home_controller.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 
 class HomeView extends GetView<HomeController> {
   HomeView({Key? key}) : super(key: key);
   final themeData = Get.find<ThemeController>().themeData.value;
   final UsStatesController usStatesController = Get.find();
-  
+
   @override
   Widget build(BuildContext context) {
+    //set the status bar color to teal
+    FlutterStatusbarcolor.setStatusBarColor(
+      const Color(0xFF016A70),
+    );
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: const SystemUiOverlayStyle(
         statusBarColor: Color(0xFF016A70),
